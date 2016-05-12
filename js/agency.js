1701d6b3c9fe8e1c3fba0e850b0a278e1069049f
“Benjamski” <benjadamski@gmail.com>
2016-05-12 08:15:52 -0400

    Added topscroll button at bottom
    -created html element with back-top class
    -css buttons
    -js click event

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,26 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+// ==============================================
+// Back To Top Button
+// ===============================================
+
+  $(window).scroll(function () {
+   // debugger;
+            if ($(this).scrollTop() > 50) {
+                $('.back-top').fadeIn();
+            } else {
+                $('.back-top').fadeOut();
+            }
+        });
+      // scroll body to 0px on click
+      $('.back-top').click(function () {
+          $('.back-top a').tooltip('hide');
+          $('body,html').animate({
+              scrollTop: 0
+          }, 800);
+          return false;
+      });
+
+      $('.back-top').tooltip('hide');
